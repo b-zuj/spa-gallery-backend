9faf3efa7dd8210d6281c6f1ab979310da75e35c
salt-dev <salt-dev@Salt-Admins-MacBook-Pro.local>
2020-10-21 11:47:42 +0200

    tests done

---
+++ api.e2e.test.js
@@ -0,0 +1,29 @@
+/* eslint-disable no-restricted-syntax */
+const request = require('supertest');
+const { createServer } = require('http');
+const { app } = require('./app.js');
+
+describe('API', () => {
+  let server;
+
+  beforeEach(() => {
+    server = createServer(app);
+    server.listen(0);
+  });
+
+  afterEach(done => {
+    server.close(done);
+  });
+
+  test('expect 200', async () => {
+    await request(server)
+      .get('/api/unsplash?query=cat')
+      .expect(200);
+  });
+
+  test('expect 404', async () => {
+    await request(server)
+      .get('/api/unspl')
+      .expect(404);
+  });
+});
---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const routes = require('./routes');
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  if (req.method === 'OPTIONS') return res.send();
+  return next();
+});
+
+app.use('/api', routes);
+
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  if (res.headersSent) {
+    return next(err);
+  }
+  res.status(err.status || 500);
+  return res.json({
+    error: {
+      message: err.message,
+    },
+  });
+});
+
+module.exports.app = app;
